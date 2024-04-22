1529388a26279fa0a4da83b6a40e22886300ed76
kurk4m <dwiemy@protonmail.com>
2024-04-22 23:47:11 +0200

    Update c4

--- store.go
+++ store.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		Pathname: strings.Join(paths, "/"),
+		Original: hashStr,
+	}
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	Pathname string
+	Original string
+}
+
+func (p PathKey) Filename() string {
+	return fmt.Sprintf("%s/%s", p.Pathname, p.Original)
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -26,15 +59,24 @@ func NewStore(opts StoreOpts) *Store {
 }
 
 func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key)
-
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	pathKey := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathKey.Pathname, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
+	pathAndFilename := pathKey.Filename()
 
-	f, err := os.Open(pathName + "/" + filename)
+	f, err := os.Create(pathAndFilename)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s", n, pathAndFilename)
 
 	return nil
 }
--- store_test.go
+++ store_test.go
@@ -1,8 +1,35 @@
 package main
 
-func TestStore(t *testing.T){
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "bestpicture"
+	pathname := CASPathTransformFunc(key)
+	fmt.Println(pathname)
+	// expectPathName := "71056/ad8aa/24742/ea41e/a36fa/2e345/2a316/36e82"
+
+	// if pathname != expectPathName {
+	// 	t.Errorf("have %s want %s", pathname, expectPathName)
+	// }
+
+	// if pathname != expectPathName {
+	// 	t.Errorf("have %s want %s", pathname, expectPathName)
+	// }
+
+}
+
+func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: ,
+		PathTransformFunc: CASPathTransformFunc,
 	}
-	s := NewStore()
+	s := NewStore(opts)
+
+	data := bytes.NewReader([]byte("some jpg bytes"))
+	if err := s.writeStream("myspecialpicture", data); err != nil {
+		t.Error(err)
+	}
 }
